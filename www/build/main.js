15f372e6838a91cd07ad2fc3baed843fac009dea
cicmpsarl <cicmp.sarl@gmail.com>
2018-07-18 04:29:28 +0200

    modif4

--- www/build/main.js
+++ www/build/main.js
@@ -4,62 +4,6 @@ webpackJsonp([2],{
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return FirstPage; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(29);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__payement_payement__ = __webpack_require__(101);
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-
-
-
-/**
- * Generated class for the FirstPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-var FirstPage = /** @class */ (function () {
-    function FirstPage(navCtrl, navParams) {
-        this.navCtrl = navCtrl;
-        this.navParams = navParams;
-        this.items = [
-            { title: 'Lorem ipsum dolor sit amet', text: 'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore  ', url: " assets/image4.jpg" },
-            { title: 'Lorem ipsum dolor sit amet', text: 'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et  ', url: " assets/image1.jpg" },
-            { title: 'Lorem ipsum dolor sit amet', text: 'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et ', url: " assets/image2.jpg" },
-            { title: 'Lorem ipsum dolor sit amet', text: 'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et ', url: " assets/image5.jpg" }
-        ];
-    }
-    FirstPage.prototype.ionViewDidLoad = function () {
-        console.log('ionViewDidLoad FirstPage');
-    };
-    FirstPage.prototype.formulaire = function () {
-        this.navCtrl.push(__WEBPACK_IMPORTED_MODULE_2__payement_payement__["a" /* PayementPage */]);
-    };
-    FirstPage = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-first',template:/*ion-inline-start:"C:\Users\erico\angular4app\lehub\hub\src\pages\first\first.html"*/'<!--\n  Generated template for the FirstPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar color="primary">\n    <ion-title class="titre2">AMERICAN DV LOTTERY 2019, WINNERS PAGE</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n    \n    <ion-grid>\n      <ion-row class="photo">\n        <ion-col col-12 col-md-12 offset-md col-lg-11 offset-lg-1 col-xl-11 offset-xl-1 >\n            <ion-img width="1400" height="300" src="assets/image7.png"></ion-img>\n        </ion-col>\n      </ion-row>\n      <ion-row class="titre">\n        <ion-col offset-5>\n          <h5 >WELCOME TO ALL WINNERS </h5>\n        </ion-col>\n      </ion-row>\n      <ion-row>\n        <ion-col col-12 col-md-6  col-lg-8 offset-lg-2  col-xl-8 offset-xl-2>\n          <p>\n          You are one of the winners randomly selected by a computer, out of the 13.5 millions entries registered for the Diversity Immigrant Visa Program 2019.<br> It is a United stated congressionally mandated lottery program for receiving a United Stated Resident Card. Also known as Green Card Lottery.\n\n          </p>\n\n        </ion-col>\n          <ion-col offset-5>\n           <button color="danger" ion-button (click)="formulaire()">click here to proceed</button>\n          </ion-col>\n        </ion-row>\n        <ion-row>\n          <ion-col col-12 col-md-6 col-lg-4 col-xl-3 *ngFor="let item of items">\n            <ion-card>\n            <!--  <ion-card-header>{{ item.title }}</ion-card-header>\n              <ion-card-content>{{ item.text }}</ion-card-content>\n            -->\n              <img [src]="item.url">\n            </ion-card>\n          </ion-col>\n        </ion-row>\n      </ion-grid>\n\n</ion-content>\n'/*ion-inline-end:"C:\Users\erico\angular4app\lehub\hub\src\pages\first\first.html"*/,
-        }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["i" /* NavParams */]])
-    ], FirstPage);
-    return FirstPage;
-}());
-
-//# sourceMappingURL=first.js.map
-
-/***/ }),
-
-/***/ 101:
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return PayementPage; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(29);
@@ -215,7 +159,9 @@ var PayementPage = /** @class */ (function () {
                     var headers = new __WEBPACK_IMPORTED_MODULE_4__angular_http__["a" /* Headers */]();
                     // headers.append('Content-Type', 'application/x-www-form-urlencoded');
                     headers.append('Content-Type', 'application/json');
-                    _this.http.post('http://localhost:3000/payer', JSON.stringify(result), { headers: headers }).map(function (res) { return res.json(); }).subscribe(function (data) {
+                    // this.http.post('http://localhost:3000/payer',  JSON.stringify(result), {headers: headers} )
+                    _this.http.post('http://ec2-18-222-66-128.us-east-2.compute.amazonaws.com:3000/payer', JSON.stringify(result), { headers: headers })
+                        .map(function (res) { return res.json(); }).subscribe(function (data) {
                         console.log(data.message);
                         alert(data.message + "\n" +
                             data.message2);
@@ -334,6 +280,62 @@ var PayementPage = /** @class */ (function () {
 
 /***/ }),
 
+/***/ 101:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return FirstPage; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(29);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__payement_payement__ = __webpack_require__(100);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+/**
+ * Generated class for the FirstPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+var FirstPage = /** @class */ (function () {
+    function FirstPage(navCtrl, navParams) {
+        this.navCtrl = navCtrl;
+        this.navParams = navParams;
+        this.items = [
+            { title: 'Lorem ipsum dolor sit amet', text: 'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore  ', url: " assets/image4.jpg" },
+            { title: 'Lorem ipsum dolor sit amet', text: 'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et  ', url: " assets/image1.jpg" },
+            { title: 'Lorem ipsum dolor sit amet', text: 'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et ', url: " assets/image2.jpg" },
+            { title: 'Lorem ipsum dolor sit amet', text: 'consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et ', url: " assets/image5.jpg" }
+        ];
+    }
+    FirstPage.prototype.ionViewDidLoad = function () {
+        console.log('ionViewDidLoad FirstPage');
+    };
+    FirstPage.prototype.formulaire = function () {
+        this.navCtrl.push(__WEBPACK_IMPORTED_MODULE_2__payement_payement__["a" /* PayementPage */]);
+    };
+    FirstPage = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'page-first',template:/*ion-inline-start:"C:\Users\erico\angular4app\lehub\hub\src\pages\first\first.html"*/'<!--\n  Generated template for the FirstPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar color="primary">\n    <ion-title class="titre2">AMERICAN DV LOTTERY 2019, WINNERS PAGE</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n    \n    <ion-grid>\n      <ion-row class="photo">\n        <ion-col col-12 col-md-12 offset-md col-lg-11 offset-lg-1 col-xl-11 offset-xl-1 >\n            <ion-img width="1400" height="300" src="assets/image7.png"></ion-img>\n        </ion-col>\n      </ion-row>\n      <ion-row class="titre">\n        <ion-col offset-5>\n          <h5 >WELCOME TO ALL WINNERS </h5>\n        </ion-col>\n      </ion-row>\n      <ion-row>\n        <ion-col col-12 col-md-6  col-lg-8 offset-lg-2  col-xl-8 offset-xl-2>\n          <p>\n          You are one of the winners randomly selected by a computer, out of the 13.5 millions entries registered for the Diversity Immigrant Visa Program 2019.<br> It is a United stated congressionally mandated lottery program for receiving a United Stated Resident Card. Also known as Green Card Lottery.\n\n          </p>\n\n        </ion-col>\n          <ion-col offset-5>\n           <button color="danger" ion-button (click)="formulaire()">click here to proceed</button>\n          </ion-col>\n        </ion-row>\n        <ion-row>\n          <ion-col col-12 col-md-6 col-lg-4 col-xl-3 *ngFor="let item of items">\n            <ion-card>\n            <!--  <ion-card-header>{{ item.title }}</ion-card-header>\n              <ion-card-content>{{ item.text }}</ion-card-content>\n            -->\n              <img [src]="item.url">\n            </ion-card>\n          </ion-col>\n        </ion-row>\n      </ion-grid>\n\n</ion-content>\n'/*ion-inline-end:"C:\Users\erico\angular4app\lehub\hub\src\pages\first\first.html"*/,
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["i" /* NavParams */]])
+    ], FirstPage);
+    return FirstPage;
+}());
+
+//# sourceMappingURL=first.js.map
+
+/***/ }),
+
 /***/ 112:
 /***/ (function(module, exports) {
 
@@ -356,11 +358,11 @@ webpackEmptyAsyncContext.id = 112;
 
 var map = {
 	"../pages/first/first.module": [
-		276,
+		277,
 		1
 	],
 	"../pages/payement/payement.module": [
-		277,
+		276,
 		0
 	]
 };
@@ -406,8 +408,8 @@ Object(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* pl
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__ionic_native_status_bar__ = __webpack_require__(197);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__app_component__ = __webpack_require__(273);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_home_home__ = __webpack_require__(274);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__pages_first_first__ = __webpack_require__(100);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__pages_payement_payement__ = __webpack_require__(101);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__pages_first_first__ = __webpack_require__(101);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__pages_payement_payement__ = __webpack_require__(100);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__angular_http__ = __webpack_require__(155);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__ionic_native_stripe__ = __webpack_require__(275);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -442,8 +444,8 @@ var AppModule = /** @class */ (function () {
                 __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
                 __WEBPACK_IMPORTED_MODULE_2_ionic_angular__["e" /* IonicModule */].forRoot(__WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* MyApp */], {}, {
                     links: [
-                        { loadChildren: '../pages/first/first.module#FirstPageModule', name: 'FirstPage', segment: 'first', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/payement/payement.module#PayementPageModule', name: 'PayementPage', segment: 'payement', priority: 'low', defaultHistory: [] }
+                        { loadChildren: '../pages/payement/payement.module#PayementPageModule', name: 'PayementPage', segment: 'payement', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/first/first.module#FirstPageModule', name: 'FirstPage', segment: 'first', priority: 'low', defaultHistory: [] }
                     ]
                 }),
                 __WEBPACK_IMPORTED_MODULE_9__angular_http__["c" /* HttpModule */],
@@ -477,7 +479,7 @@ var AppModule = /** @class */ (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(29);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__ = __webpack_require__(197);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(195);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_first_first__ = __webpack_require__(100);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_first_first__ = __webpack_require__(101);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
